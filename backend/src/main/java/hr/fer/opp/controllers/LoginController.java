ef144dddea23f8b85072b705f8810b8f5dfaa33d
mturina <marko.turina@fer.hr>
2019-10-23 18:26:56 +0200

    Added login controller and login service

---
+++ backend/src/main/java/hr/fer/opp/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package hr.fer.opp.controllers;
+
+import hr.fer.opp.model.Admin;
+import hr.fer.opp.model.User;
+import hr.fer.opp.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @GetMapping(value="/login")
+    public String loginUser(@RequestBody User user) {
+        return loginService.checkUser(user);
+    }
+
+    @PostMapping(value="/register")
+    public String registerUser(@RequestBody User user) {
+        return loginService.registerUser(user);
+    }
+
+}

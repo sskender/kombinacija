6e979740d5cbdacaa78ecb28d3e647eb71d6894b
sskender <sven.skender@fer.hr>
2019-10-27 16:27:51 +0100

    basic user renamed to citizen

--- backend/src/main/java/hr/fer/opp/controllers/LoginController.java
+++ backend/src/main/java/hr/fer/opp/controllers/LoginController.java
@@ -1,10 +1,11 @@
 package hr.fer.opp.controllers;
 
-import hr.fer.opp.model.Admin;
-import hr.fer.opp.model.User;
+import hr.fer.opp.model.Citizen;
 import hr.fer.opp.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
@@ -13,13 +14,13 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping(value="/login")
-    public String loginUser(@RequestBody User user) {
-        return loginService.checkUser(user);
+    public String loginUser(@RequestBody Citizen citizen) {
+        return loginService.checkUser(citizen);
     }
 
     @PostMapping(value="/register")
-    public String registerUser(@RequestBody User user) {
-        return loginService.registerUser(user);
+    public String registerUser(@RequestBody Citizen citizen) {
+        return loginService.registerUser(citizen);
     }
 
 }

2d7eac7afb5a68c8daf2757af5a632804c7bd896
sskender <sven.skender@fer.hr>
2019-10-27 18:12:33 +0100

    Controllers v1

---
+++ backend/src/main/java/hr/fer/opp/controllers/AdminPanelController.java
@@ -0,0 +1,14 @@
+package hr.fer.opp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminPanelController {
+
+    // crud ops for employee
+
+    // crud ops for neighborhood
+
+    // crud ops for trash cans
+
+}
---
+++ backend/src/main/java/hr/fer/opp/controllers/PublicUserController.java
@@ -0,0 +1,10 @@
+package hr.fer.opp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublicUserController {
+
+    // return trascans for given date
+
+}

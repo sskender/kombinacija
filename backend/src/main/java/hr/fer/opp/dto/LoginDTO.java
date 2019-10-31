66558305c033c576a0a61b266a966c2c2204496c
mbicanic <miroslav.bicanic@fer.hr>
2019-10-31 15:12:40 +0100

    created example DTO objects

---
+++ backend/src/main/java/hr/fer/opp/dto/LoginDTO.java
@@ -0,0 +1,27 @@
+package hr.fer.opp.dto;
+
+public class LoginDTO {
+    private String email;
+    private String pwd;
+
+    public LoginDTO() {}
+    public LoginDTO(String email, String pwd){
+        this.email = email;
+        this.pwd = pwd;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
---
+++ backend/src/main/java/hr/fer/opp/dto/RegisterDTO.java
@@ -0,0 +1,44 @@
+package hr.fer.opp.dto;
+
+public class RegisterDTO {
+    private String name;
+    private String lastName;
+    private String email;
+    private String pwd;
+
+    public RegisterDTO(){}
+    public RegisterDTO(String name, String lastName, String email, String pwd){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}

c4ed2f213a857c276a25561fae78d9509f61f3e8
mbicanic <miroslav.bicanic@fer.hr>
2019-11-07 19:27:41 +0100

    removed login method, refactored registration

--- backend/src/main/java/hr/fer/opp/controllers/LoginController.java
+++ backend/src/main/java/hr/fer/opp/controllers/LoginController.java
@@ -1,9 +1,10 @@
 package hr.fer.opp.controllers;
 
-import hr.fer.opp.dto.LoginDTO;
 import hr.fer.opp.dto.RegisterDTO;
+import hr.fer.opp.model.Citizen;
 import hr.fer.opp.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,17 +15,11 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @PostMapping(value="/login")
-    public String loginUser(@RequestBody LoginDTO loginDTO) {
-        // user hash password
-        // return loginService.checkUser(citizen);
-        return null;
-    }
-
     @PostMapping(value="/register")
-    public String registerUser(@RequestBody RegisterDTO registerDTO) {
-        //return loginService.registerUser(citizen);
-        return null;
-    }
+    public Citizen registerUser(@RequestBody RegisterDTO registerDTO) {
 
+        System.out.println("####################### REST ########################");
+        registerDTO.setPwd(new BCryptPasswordEncoder().encode(registerDTO.getPwd()));
+        return loginService.registerUser(registerDTO);
+    }
 }

b5eacd27d15624ffaac6d5913a1214def65101b2
sskender <sven.skender@fer.hr>
2019-11-07 21:05:19 +0100

    person service interace and impl refactor

--- backend/src/main/java/hr/fer/opp/services/PersonService.java
+++ backend/src/main/java/hr/fer/opp/services/PersonService.java
@@ -1,39 +1,15 @@
 package hr.fer.opp.services;
 
-import hr.fer.opp.dao.AdminRepository;
-import hr.fer.opp.dao.CitizenRepository;
-import hr.fer.opp.dao.EmployeeRepository;
-import hr.fer.opp.dao.PersonRepository;
 import hr.fer.opp.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class PersonService {
+public interface PersonService {
 
-    @Autowired
-    PersonRepository personRepository;
-    @Autowired
-    CitizenRepository citizenRepository;
-    @Autowired
-    EmployeeRepository employeeRepository;
-    @Autowired
-    AdminRepository adminRepository;
+    Person fetchByEmail(String email);
 
-    public Person fetchByEmail(String email){
-        return personRepository.findByEmail(email).orElseThrow(
-                ()-> new RuntimeException());
-    }
+    boolean isAdmin(Long id);
 
-    public boolean isAdmin(Long id){
-        return adminRepository.findById(id).isPresent();
-    }
+    boolean isEmployee(Long id);
 
-    public boolean isEmployee(Long id){
-        return employeeRepository.findById(id).isPresent();
-    }
+    boolean isCitizen(Long id);
 
-    public boolean isCitizen(Long id){
-        return citizenRepository.findById(id).isPresent();
-    }
 }
---
+++ backend/src/main/java/hr/fer/opp/services/impl/PersonServiceImpl.java
@@ -0,0 +1,48 @@
+package hr.fer.opp.services.impl;
+
+import hr.fer.opp.dao.AdminRepository;
+import hr.fer.opp.dao.CitizenRepository;
+import hr.fer.opp.dao.EmployeeRepository;
+import hr.fer.opp.dao.PersonRepository;
+import hr.fer.opp.model.Person;
+import hr.fer.opp.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private CitizenRepository citizenRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Override
+    public Person fetchByEmail(String email) {
+        return personRepository.findByEmail(email).orElseThrow(
+                RuntimeException::new);
+    }
+
+    @Override
+    public boolean isAdmin(Long id) {
+        return adminRepository.findById(id).isPresent();
+    }
+
+    @Override
+    public boolean isEmployee(Long id) {
+        return employeeRepository.findById(id).isPresent();
+    }
+
+    @Override
+    public boolean isCitizen(Long id) {
+        return citizenRepository.findById(id).isPresent();
+    }
+
+}

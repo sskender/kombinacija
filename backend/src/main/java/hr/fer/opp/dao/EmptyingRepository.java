5d8d5f6d9f64079d7f2fb1548ff4d09c2fbbde63
sskender <sven.skender@fer.hr>
2019-11-07 20:49:44 +0100

    Added Repository tag

--- backend/src/main/java/hr/fer/opp/dao/EmptyingRepository.java
+++ backend/src/main/java/hr/fer/opp/dao/EmptyingRepository.java
@@ -2,6 +2,8 @@ package hr.fer.opp.dao;
 
 import hr.fer.opp.model.Emptying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmptyingRepository extends JpaRepository<Emptying, Long> {
 }

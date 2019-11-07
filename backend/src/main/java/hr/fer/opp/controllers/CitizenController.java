c164faa881115c8adb65d0d379fe1fd30a2e9c1d
mbicanic <miroslav.bicanic@fer.hr>
2019-11-07 19:27:05 +0100

    added test method for full ping mapping

--- backend/src/main/java/hr/fer/opp/controllers/CitizenController.java
+++ backend/src/main/java/hr/fer/opp/controllers/CitizenController.java
@@ -1,15 +1,19 @@
 package hr.fer.opp.controllers;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CitizenController {
 
-    // trash can management (empty, full, extra full)
-
-    // favorite trash (crud)
-
-
-    // + upload ???
+    @PostMapping(value="/trash/{id}/full")
+    @Secured("CITIZEN")
+    public String pingFull(@PathVariable("id") Long id){
+        System.out.println("ULETIOOOOOOOOOOOO");
+        return "ping successful";
+    }
 
 }

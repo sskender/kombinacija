bda84e460e33bb4f921d64919816f76bdd02de56
sskender <sven.skender@fer.hr>
2019-11-07 21:01:27 +0100

    admin service interace and impl refactor

--- backend/src/main/java/hr/fer/opp/services/AdminService.java
+++ backend/src/main/java/hr/fer/opp/services/AdminService.java
@@ -1,17 +1,6 @@
 package hr.fer.opp.services;
 
-import hr.fer.opp.dao.AdminRepository;
-import hr.fer.opp.model.Admin;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-public class AdminService {
-
-    @Autowired
-    private AdminRepository adminRepository;
+public interface AdminService {
 
     /**
      * Checks if there is an account with the given e-mail
@@ -19,8 +8,6 @@ public class AdminService {
      * @param email the User received from the front-end
      * @return
      */
-    public boolean isAdmin(String email){
-        Optional<Admin> opt = adminRepository.findByEmail(email);
-        return opt.isPresent();
-    }
+    boolean isAdmin(String email);
+
 }

36eec97988bfd09e1096ef5b2b3b18856c951788
sskender <sven.skender@fer.hr>
2019-12-04 12:17:15 +0100

    removed unused methods from admin service

--- backend/src/main/java/hr/fer/opp/services/AdminService.java
+++ backend/src/main/java/hr/fer/opp/services/AdminService.java
@@ -2,13 +2,6 @@ package hr.fer.opp.services;
 
 public interface AdminService {
 
-    /**
-     * Checks if there is an account with the given e-mail
-     * in the admin table
-     *
-     * @param email the User received from the front-end
-     * @return
-     */
-    boolean isAdmin(String email);
+    
 
 }

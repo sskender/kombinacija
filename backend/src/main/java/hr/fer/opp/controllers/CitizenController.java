7e1982b05d5d862752c6b908ca3e534031f05f7b
mturina <marko.turina@fer.hr>
2019-12-11 18:24:23 +0100

    Citizen controller and implementation

--- backend/src/main/java/hr/fer/opp/controllers/CitizenController.java
+++ backend/src/main/java/hr/fer/opp/controllers/CitizenController.java
@@ -1,11 +1,17 @@
 package hr.fer.opp.controllers;
 
+import hr.fer.opp.dto.response.FavoriteREST;
+import hr.fer.opp.dto.response.PingREST;
 import hr.fer.opp.services.CitizenService;
 import hr.fer.opp.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -18,46 +24,46 @@ public class CitizenController {
     private CitizenService citizenService;
 
     @PostMapping(value = "/ping/{id}/f")
-    public String pingFull(@PathVariable("id") Long id) {
-        // TODO
-        return "Pinging container " + id.toString() + " as full";
+    public ResponseEntity<PingREST> pingFull(@PathVariable("id") Long id,
+                                             @AuthenticationPrincipal UserDetails userDetails) {
+        return new ResponseEntity<>(new PingREST(citizenService.pingContainerFull(id, personService.fetchByEmail(userDetails.getUsername()))),
+                HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/ping/{id}/u")
-    public String pingUrgent(@PathVariable("id") Long id) {
-        // TODO
-        return "Pinging container " + id.toString() + " as urgent";
+    public ResponseEntity<PingREST> pingUrgent(@PathVariable("id") Long id,
+                                               @AuthenticationPrincipal UserDetails userDetails) {
+        return new ResponseEntity<>(new PingREST(citizenService.pingContainerUrgent(id, personService.fetchByEmail(userDetails.getUsername()))),
+                HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/ping/{id}/e")
-    public String pingEmpty(@PathVariable("id") Long id) {
-        // TODO
-        return "Pinging container " + id.toString() + " as empty";
+    public ResponseEntity<PingREST> pingEmpty(@PathVariable("id") Long id,
+                                              @AuthenticationPrincipal UserDetails userDetails) {
+        return  new ResponseEntity<>(new PingREST(citizenService.pingContainerEmpty(id, personService.fetchByEmail(userDetails.getUsername()))),
+                HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/favorite")
-    public String favorites() {
-        // TODO
-        return "Listing all favorites";
+    public List<FavoriteREST> favorites(@AuthenticationPrincipal UserDetails userDetails) {
+        return FavoriteREST.convertToREST(citizenService.getFavoriteContainers(personService.fetchByEmail(userDetails.getUsername())));
     }
 
     @PostMapping(value = "/favorite/{id}")
-    public String addFavorite(
+    public ResponseEntity<FavoriteREST> addFavorite(
             @PathVariable("id") Long contID,
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        // TODO
-        // TODO error crash if user = null
-        return "Adding container " + contID + " as " + userDetails.getUsername() + "'s favorite";
+        return new ResponseEntity<>(new FavoriteREST(citizenService.addToFavorites(contID, personService.fetchByEmail(userDetails.getUsername()))),
+                HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/favorite/{id}")
-    public String deleteFavorite(
+    public boolean deleteFavorite(
             @PathVariable("id") Long contID,
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        // TODO
-        return "Removing container " + contID + " as " + userDetails.getUsername() + "'s favorite";
+        return citizenService.removeFromFavorites(contID, personService.fetchByEmail(userDetails.getUsername()));
     }
 
 }
--- backend/src/main/java/hr/fer/opp/services/impl/CitizenServiceImpl.java
+++ backend/src/main/java/hr/fer/opp/services/impl/CitizenServiceImpl.java
@@ -1,35 +1,127 @@
 package hr.fer.opp.services.impl;
 
+import hr.fer.opp.dao.ContainerRepository;
+import hr.fer.opp.dao.FavoriteRepository;
+import hr.fer.opp.dao.PersonRepository;
+import hr.fer.opp.dao.PingRepository;
+import hr.fer.opp.exceptions.RequestDeniedException;
+import hr.fer.opp.model.Container;
 import hr.fer.opp.model.Favorite;
 import hr.fer.opp.model.Person;
 import hr.fer.opp.model.Ping;
 import hr.fer.opp.model.enums.PingLevel;
 import hr.fer.opp.services.CitizenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CitizenServiceImpl implements CitizenService {
 
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+
+    @Autowired
+    private ContainerRepository containerRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private PingRepository pingRepository;
+
     @Override
     public Favorite addToFavorites(Long containerId, Person owner) {
-        return null;
+        Favorite f = new Favorite();
+
+        Optional<Container> c = containerRepository.findById(containerId);
+
+        if(c.isPresent()) {
+            f.setContainer(c.get());
+        } else {
+            throw new RequestDeniedException("Can't register given container as a favorite. Container with the given id does not exist.");
+        }
+
+        Optional<Person> p = personRepository.findById(owner.getId());
+
+        if(p.isPresent()) {
+            f.setOwner(p.get());
+        } else {
+            throw new RequestDeniedException("Person with the given id does not exist.");
+        }
+
+        c.get().getFavorites().add(f);
+        p.get().getFavorites().add(f);
+
+        return favoriteRepository.save(f);
     }
 
     @Override
     public boolean removeFromFavorites(Long containerId, Person owner) {
-        return false;
+        Optional<Container> c = containerRepository.findById(containerId);
+        Optional<Person> p = personRepository.findById(owner.getId());
+
+
+        if (!c.isPresent()) {
+            throw new RequestDeniedException("Container with given ID does not exist.");
+        }
+
+        if (!p.isPresent()) {
+            throw new RequestDeniedException("Person with given ID does not exist.");
+        }
+
+        List<Favorite> favorites = p.get().getFavorites();
+
+        for(Favorite f : favorites) {
+            if (f.getContainer().getId() == c.get().getId()) {
+                favoriteRepository.delete(f);
+                return true;
+            }
+        }
+        throw new RequestDeniedException("Favorite does not exist.");
     }
 
     @Override
     public List<Favorite> getFavoriteContainers(Person owner) {
-        return null;
+        Optional<Person> p = personRepository.findById(owner.getId());
+
+        if(p.isPresent()) {
+            return favoriteRepository.findAll().stream().filter(f -> f.getOwner().getId() == p.get().getId()).collect(Collectors.toList());
+        } else {
+            throw new RequestDeniedException("Person with given Id does not exist.");
+        }
     }
 
     @Override
     public Ping pingContainer(Long containerId, Person creator, PingLevel pingLevel) {
-        return null;
+        Optional<Container> c = containerRepository.findById(containerId);
+        Optional<Person> p = personRepository.findById(creator.getId());
+        Ping ping = new Ping();
+
+        if (p.isPresent()) {
+            ping.setCreator(p.get());
+        } else {
+            throw new RequestDeniedException("Person with given ID does not exist.");
+        }
+
+        if (c.isPresent()) {
+            ping.setContainer(c.get());
+        } else {
+            throw new RequestDeniedException("Container with given ID does not exist.");
+        }
+
+
+        ping.setLevel(pingLevel);
+        ping.setTimestamp(0);
+        ping.setPhotoPath("");
+
+        p.get().getPings().add(ping);
+        c.get().getPings().add(ping);
+
+        return pingRepository.save(ping);
     }
 
 }

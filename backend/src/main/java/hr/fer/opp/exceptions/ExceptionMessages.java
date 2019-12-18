b84a1a8ab5ce1775fc4d80f5178892f750e58aa3
mtropcic <mario.tropcic@gmail.com>
2019-12-18 21:52:09 +0100

    Extended exc messages

--- backend/src/main/java/hr/fer/opp/exceptions/ExceptionMessages.java
+++ backend/src/main/java/hr/fer/opp/exceptions/ExceptionMessages.java
@@ -2,6 +2,8 @@ package hr.fer.opp.exceptions;
 
 public abstract class ExceptionMessages {
 
+	public static final String EXCEPTION_MESSAGE_USER_EXISTS = "User with given e-mail already exists.";
+
 	public static final String EXCEPTION_MESSAGE_EMPLOYEE_NOT_EXIST = "Employee with given id does not exist";
 
 	public static final String EXCEPTION_MESSAGE_EMPLOYEE_EMAIL_NOT_EXIST = "Employee with given email does not exist";
@@ -14,15 +16,19 @@ public abstract class ExceptionMessages {
 
 	public static final String EXCEPTION_MESSAGE_EMPLOYEE_CAN_NOT_UPDATE = "Can't update given employee in requested neighborhood";
 
+	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NOT_EXIST = "Neighborhood with given id does not exist";
+
+	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NAME_NOT_EXIST = "Neighborhood with given name does not exist.";
+
+	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NAME_EXISTS = "Neighborhood with given name already exists.";
+
 	public static final String EXCEPTION_MESSAGE_CONTAINER_NOT_EXIST = "Container with given id does not exist";
 
 	public static final String EXCEPTION_MESSAGE_CONTAINER_CAN_NOT_REGISTER = "Can't register given container in requested neighborhood.";
 
 	public static final String EXCEPTION_MESSAGE_CONTAINER_CAN_NOT_UPDATE = "Can't update given container in requested neighborhood.";
 
-	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NOT_EXIST = "Neighborhood with given id does not exist";
+	public static final String EXCEPTION_MESSAGE_CONTAINER_CAN_NOT_REGISTER_FAVORITE = "Can't register given container as a favorite";
 
-	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NAME_NOT_EXIST = "Neighborhood with given name does not exist.";
-
-	public static final String EXCEPTION_MESSAGE_NEIGHBORHOOD_NAME_EXISTS = "Neighborhood with given name already exists.";
+	public static final String EXCEPTION_MESSAGE_FAVORITE_NOT_EXIST = "Favorite does not exist.";
 }

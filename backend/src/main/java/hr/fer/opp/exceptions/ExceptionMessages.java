640cd15741dee58cc8348d58640ddcb4c4e7d8fe
sskender <sven.skender@fer.hr>
2019-12-18 20:42:39 +0100

    exception messages class

--- backend/src/main/java/hr/fer/opp/exceptions/ExceptionMessages.java
+++ backend/src/main/java/hr/fer/opp/exceptions/ExceptionMessages.java
@@ -2,7 +2,8 @@ package hr.fer.opp.exceptions;
 
 public abstract class ExceptionMessages {
 
-    public static final String EXCEPTION_MESSAGE_EMPLOYEE_NOT_EXIST = "Container with given id does not exist";
+    public static final String EXCEPTION_MESSAGE_EMPLOYEE_NOT_EXIST = "Employee with given email does not exist";
+
     public static final String EXCEPTION_MESSAGE_CONTAINER_NOT_EXIST = "Container with given id does not exist";
 
 }

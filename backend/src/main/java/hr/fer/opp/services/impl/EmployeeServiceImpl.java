82aab07d3fa632dbf36afa7dff8b9560007c2bd6
sskender <sven.skender@fer.hr>
2019-12-18 16:40:33 +0100

    employee service interface updated

--- backend/src/main/java/hr/fer/opp/services/impl/EmployeeServiceImpl.java
+++ backend/src/main/java/hr/fer/opp/services/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package hr.fer.opp.services.impl;
 
-import hr.fer.opp.model.Container;
+import hr.fer.opp.dto.response.ContainerREST;
+import hr.fer.opp.dto.response.EmptyingREST;
 import hr.fer.opp.model.Person;
 import hr.fer.opp.services.EmployeeService;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,22 @@ import java.util.List;
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
-    public boolean emptyContainer(Long containerId, Person worker) {
+    public EmptyingREST emptyContainer(Long containerId, Person worker) {
+        return null;
+    }
+
+    @Override
+    public boolean reportLegitimatePing(Long containerId) {
         return false;
     }
 
     @Override
-    public List<Container> getWorkRoute(Person worker) {
+    public boolean reportFakePing(Long containerId) {
+        return false;
+    }
+
+    @Override
+    public List<ContainerREST> getWorkRoute(Person worker) {
         return null;
     }
 

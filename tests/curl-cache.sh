6ceaff32bce15ef17c997bb62a53bf1358c20ad7
sskender <sven.skender@fer.hr>
2019-12-26 21:24:23 +0100

    curl caching tests

---
+++ tests/curl-cache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# css
+curl -I http://kombinacija.hopto.org/css/style.css
+
+# html
+curl -I http://kombinacija.hopto.org
+
+# images
+curl -I http://kombinacija.hopto.org/img/ManGarbage.jpg
+
+# js api
+curl -I http://kombinacija.hopto.org/js/auth.js
+
+# js bootstrap
+curl -I http://kombinacija.hopto.org/vendor/bootstrap/js/bootstrap.bundle.js

8c6aa14e95badb845c3e75bec04671cccb8e9d71
mbicanic <miroslav.bicanic@fer.hr>
2020-01-08 12:55:48 +0100

    patched headers

--- backend/src/main/java/hr/fer/opp/security/WebSecurityCorsFilter.java
+++ backend/src/main/java/hr/fer/opp/security/WebSecurityCorsFilter.java
@@ -21,15 +21,16 @@ public class WebSecurityCorsFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse)response;
         HttpServletRequest req = (HttpServletRequest)request;
-        if(req.getMethod().equals("OPTIONS")){
-            res.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
+
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         res.setHeader("Access-Control-Allow-Origin", "*");
+        if(req.getMethod().equals("OPTIONS")){
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
         chain.doFilter(request, res);
     }
